0117832380f48900e949058ba4fc7e7fbe18e347
IntelliSoftDev <ahowson85@gmail.com>
2015-09-11 17:54:30 -0400

    A scanner application for TWAIN and WIA scanners for Wiindows 8/10

---
+++ NgScanApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NgScanApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            getDevices();
+        }
+        private void getDevices()
+        {
+            try
+            {
+                List<string> devices = WIAScanner.GetDevices();
+
+                foreach(string device in devices)
+                {
+                    DeviceCmb.Items.Add(device);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Unexpected Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void ScanBtn_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
